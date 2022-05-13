07860f2ae2807b7677dc1692797819e2662de09e
Bartosz Nowacki <nowackib@protonmail.com>
2022-05-13 13:19:30 +0200

    clear code

--- app/Game.js
+++ app/Game.js
@@ -1,11 +1,9 @@
 import { Quote } from "./Quote.js";
 
 class Game {
-  //15 dodawanie obrazkow
   currentStep = 0;
   lastStep = 7;
 
-  //7robimy tabliec w ktorej są obiekty 7
   quotes = [
     {
       text: "pulp fiction",
@@ -46,7 +44,6 @@ class Game {
     },
   ];
 
-  //4 mogł”ym tutaj wymienić jeden obiekt i odebrać wszystko jako jeden obiekt, ale js umozliwia, wiec  tworze obiekt za pomocą {} i wypakowuje wszystkie i tu je odbieram
   constructor({
     lettersWrapper,
     categoryWrapper,
@@ -54,32 +51,23 @@ class Game {
     outputWrapper,
     btn,
   }) {
-    // 5jezeli uzywamy tych argumentów wyżej, to w ramach konstruktora, moglbym je uzywać. wyswietlic je, oddac robić co chce. musimy zrobić tak aby te argumenty stały się własciwosciami klasy Game
-    //odebrałem w konstruktorze 4 argumenty te wyżej i przypisałem je do obiektu5
     this.lettersWrapper = lettersWrapper;
     this.categoryWrapper = categoryWrapper;
     this.wordWrapper = wordWrapper;
     this.outputWrapper = outputWrapper;
     this.btn = btn;
 
-    //8 dodajemy tekst wyswietlany
     const { text, category } =
       this.quotes[Math.floor(Math.random() * this.quotes.length)];
 
-    //9przypisujemy kategorie do wrappera
     this.categoryWrapper.innerHTML = category;
 
-    //10 text00
     this.quote = new Quote(text);
   }
 
-  //6
   guess(letter, event) {
-    //14 nciskanie przycisku tylko raz
     event.target.disabled = true;
 
-    //13znajdowanie liter13
-    //15 dodawanie obrazkow
     if (this.quote.guess(letter)) {
       this.drawQuote();
     } else {
@@ -88,38 +76,24 @@ class Game {
         this.currentStep
       ].style.opacity = 1;
 
-      //16 wygrana przegrana
       if (this.currentStep == this.lastStep) {
         this.loosing();
       }
     }
   }
   drawLetters() {
-    //6 w momencie startu chce do this.lettersWrapper wstawić litery
     for (let i = 0; i < 26; i++) {
       const label = (i + 10).toString(36);
       const button = document.createElement("button");
-
-      //zawartosc buttona
       button.innerHTML = label;
-
-      //chce zeby button jako argmuent odbierał label, wiec robimy mały closer. czyli po kliknięciu ja z tej konkretnej metody game  chce wywolac metode guess (czyli zgadnij )
       button.addEventListener("click", (event) => this.guess(label, event));
-
-      //do tego wrappera bede dodawac dzieci ,ktore mają być buttonami6
       this.lettersWrapper.appendChild(button);
     }
   }
 
   drawQuote() {
-    // 12musze pobrac tekst z podresleniami
-
     const content = this.quote.getContent();
-
-    //dalej wkladamy kontent do wrappera
     this.wordWrapper.innerHTML = content;
-
-    //16 wygrana/przegrana
     if (!content.includes("_")) {
       this.winning();
     }
@@ -151,16 +125,13 @@ class Game {
     this.drawQuote();
   }
 
-  //metoda powinna robic jedna rzecz
   start() {
-    //15 dodawanie obrazkow
     document.getElementsByClassName("step")[this.currentStep].style.opacity = 1;
     this.drawLetters();
     this.drawQuote();
     this.buttonReset();
   }
 
-  //16 wygrana przegrana
   winning() {
     this.wordWrapper.innerHTML = "Congratulations! You are a winner! :)";
     this.lettersWrapper.innerHTML = "";
@@ -172,17 +143,12 @@ class Game {
   }
 }
 
-// 1przenieslismy kod czyli wywołanie oddzielnie1
-
 const game = new Game({
-  //2mowimy grze z czego ma korzystać. przekazujemy obikety 2
   lettersWrapper: document.getElementById("letters"),
   categoryWrapper: document.getElementById("category"),
   wordWrapper: document.getElementById("word"),
   outputWrapper: document.getElementById("output"),
   btn: document.getElementById("btn"),
-
-  // 3jak coś wywołujemy to musimy to również odebrać. przekazujemy to prze zkonstruktor, wiec musimy utworzyc konstruktor3
 });
 
 game.start();
--- app/Quote.js
+++ app/Quote.js
@@ -1,36 +1,28 @@
 export class Quote {
-    constructor(text) {
-        this.text = text;
-        //13 odnajdywanie liter
-        this.guessed = [];
+  constructor(text) {
+    this.text = text;
+
+    this.guessed = [];
+  }
+
+  getContent() {
+    let content = "";
+    for (const char of this.text) {
+      //13 odnajdywanie
+      if (char == " " || this.guessed.includes(char)) {
+        content += char;
+      } else {
+        content += "_";
+      }
     }
-    //zalozylismy nowa klase, bo tak jest elegancko. jednak zeby uzywac jej w innym miejscu musimy ją wykesportować słówem export
+    return content;
+  }
 
-
-    //11 zamiast kazdej litery znaczek podresleni + spacje. czyli przeporwadzamy tu interacja po tekscie 
-    getContent() {
-        let content = '';
-        for (const char of this.text) {
-            //13 odnajdywanie
-            if (char == ' ' || this.guessed.includes(char)) {
-                content += char;
-            } else {
-                content += '_';
-            }
-        }
-        return content;
-
-
+  guess(letter) {
+    if (!this.text.includes(letter)) {
+      return false;
     }
-    //13znajdowanie liter13
-    guess(letter) {
-
-
-
-        if (!this.text.includes(letter)) {
-            return false;
-        }
-        this.guessed.push(letter)
-        return true;
-    }
+    this.guessed.push(letter);
+    return true;
+  }
 }
